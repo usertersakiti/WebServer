dbbf75c3080a9e64b4ecf82b4cd7d5da1a451637
usertersakiti <51637256+usertersakiti@users.noreply.github.com>
2019-06-12 18:59:35 +0700

    Add files via upload

---
+++ webserver.sh
@@ -0,0 +1,24 @@
+#!usr/bin/bash
+
+ figlet "Web-Server"
+
+ sleep 2
+
+ echo "Project Work Membuat WebServer" 
+ echo "Nama : User_Tersakiti"
+ echo "Kelas : XI TKJ 2"
+ echo ""
+ echo "SMK Cibening"
+
+ sleep 1
+
+ echo "Installing"
+ sleep 2
+
+ apt install php -y 
+ apt install apache2 -y 
+ clear
+ mkdir ~/webserver
+ cd ~/webserver
+ php -S localhost:8080
+#User_Tersakiti
